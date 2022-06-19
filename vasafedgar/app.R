1785f684a96a1bfb3783acbc6c15a5ed10e0e397
Sölvi Rögnvaldsson <sor16@hi.is>
2022-06-19 22:53:59 +0300

    first prototype

---
+++ vasafedgar/app.R
@@ -0,0 +1,85 @@
+library(shiny)
+library(rStrava)
+library(dplyr)
+library(ggplot2)
+library(httr)
+source('account_info.R')
+source('utils.R')
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    
+    # Application title
+    titlePanel("Vasafeðgar"),
+    
+    # Sidebar with a slider input for number of bins 
+        fluidRow(
+            column(3,
+               wellPanel(
+                   h4("Stillingar"),
+                   checkboxGroupInput(inputId='athlete',label='Veldu íþróttamenn',choices = athletes,selected = athletes[1]),
+                   actionButton(inputId = "update",label = "Uppfæra")
+               ),
+               wellPanel(
+                   h4("Bæta við íþróttamanni"),
+                   textInput(inputId='name',label='Nafn'),
+                   textInput(inputId='athlete_id',label='Athlete ID'),
+                   actionButton(inputId = "authenticate",label = "Auðkenna")
+               )
+
+            ),
+            column(9,
+                plotOutput("active_minutes")
+            )
+        )
+    )
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    observeEvent(input$authenticate,{
+        stoken <- httr::config(token = strava_oauth(app_name, 
+                                                    app_client_id, 
+                                                    app_secret, 
+                                                    app_scope="activity:read_all",
+                                                    cache=T))
+        stoken_binary = paste(readBin('.httr-oauth',what = "raw",n = 1e4),collapse='')
+        dat <- c('name'=input$name,
+                 'athlete_id'=input$athlete_id,
+                 'auth'=stoken_binary)
+        save_data('User',dat)
+    })
+    
+    observeEvent(input$update,{
+        update_activities()
+        output$active_minutes <- renderPlot({
+            cols <- c('#BC3C29FF','#0072B5FF','#E18727FF','#20854EFF')
+            activities <- load_data('Activity')
+            if(nrow(activities)==0){
+                return(NULL)
+            }
+            activities %>%
+            mutate(cum_time=cumsum(time)/(60*60),
+                   date=as.Date(date)) %>%
+            ggplot(aes(date,cum_time,fill=factor(athlete_id),col=factor(athlete_id))) +
+            geom_line(size=2) +
+            geom_point(size=4) +
+            scale_color_manual(values=head(cols,nrow(users)),labels=users$name,name='Íþróttamaður') +
+            scale_fill_manual(values=head(cols,nrow(users)),labels=users$name,name='Íþróttamaður') +
+            xlab('Dagsetning') +
+            ylab('Uppsafnaður æfingatími') +
+            theme_classic()+
+            theme(
+                plot.title=element_text(size=18,face="bold"),
+                axis.title.x=element_text(size=18),
+                axis.title.y=element_text(size=18),
+                axis.text.x=element_text(size=18),
+                axis.text.y=element_text(size=18),
+                legend.title=element_text(size=18),
+                legend.text = element_text(size=18),
+                plot.margin=unit(c(0,1,0,0),'cm')
+            )
+        })
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ vasafedgar/utils.R
@@ -0,0 +1,72 @@
+library(RMySQL)
+library(httr)
+
+save_data <- function(table,data) {
+    # Connect to the database
+    db <- dbConnect(MySQL(), dbname = database_name, host = options()$mysql$host, 
+                    port = options()$mysql$port, user = options()$mysql$user, 
+                    password = options()$mysql$password)
+    # Construct the update query by looping over the data fields
+    query <- sprintf(
+        "INSERT INTO %s (%s) VALUES ('%s') AS NEW ON DUPLICATE KEY UPDATE %s",
+        table,
+        paste(names(data), collapse = ", "),
+        paste(data, collapse = "', '"),
+        paste(paste0(names(data),' = NEW.',names(data)),collapse=', ')
+    )
+    # Submit the update query and disconnect
+    dbGetQuery(db, query)
+    dbDisconnect(db)
+}
+
+load_data <- function(table) {
+    # Connect to the database
+    db <- dbConnect(MySQL(), dbname = database_name, host = options()$mysql$host, 
+                    port = options()$mysql$port, user = options()$mysql$user, 
+                    password = options()$mysql$password)
+    # Construct the fetching query
+    query <- sprintf("SELECT * FROM %s", table)
+    # Submit the fetch query and disconnect
+    data <- dbGetQuery(db, query)
+    dbDisconnect(db)
+    data
+}
+
+get_stoken_from_binary <- function(t){
+    t_bin_vec <- unlist(strsplit(t,split=''))
+    t_bin_pairs <- paste0(t_bin_vec[c(T,F)],t_bin_vec[c(F,T)])
+    f = file('.httr-oauth',"wb")
+    writeBin(object=as.raw(as.hexmode(t_bin_pairs)),con=f) 
+    close(f)
+    httr::config(token = readRDS('.httr-oauth')[[1]])
+}
+
+update_activities <- function(){
+    users <- load_data('User')
+    stokens <- list()
+    for(i in 1:nrow(users)){
+        stokens[[i]] <- get_stoken_from_binary(users$auth[i])
+        date_origin <- if(is.na(users$last_updated[i])) as.Date('2022-05-01') else as.Date(users$last_updated[i],format='%Y-%m-%d')
+        new_activities <- get_activity_list(stokens[[i]],after=date_origin-1)
+        save_data('User',data=c('name'=users$name[i],
+                                'athlete_id'=users$athlete_id[i],
+                                'auth'=users$auth[i],
+                                'photo'=users$photo[i],
+                                'last_updated'=gsub(' GMT','',.POSIXct(Sys.time(), tz="GMT"))))
+        save_activities(new_activities,users$athlete_id[i])
+    }
+}
+
+save_activities <- function(activity_list,athlete_id){
+    for(i in 1:length(activity_list)){
+        dat <- c('athlete_id'=athlete_id,
+                 'date'=gsub('T',' ',gsub('Z$','',activity_list[[i]]$start_date)),
+                 'name'=activity_list[[i]]$name,
+                 'type'=activity_list[[i]]$type,
+                 'time'=activity_list[[i]]$moving_time,
+                 'distance'=activity_list[[i]]$distance,
+                 'avg_heartrate'=if(activity_list[[i]]$has_heartrate) activity_list[[i]]$average_heartrate else 'NA',
+                 'description'='')
+        save_data('Activity',dat)
+    }
+}
